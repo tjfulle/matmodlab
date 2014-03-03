7e0c1c2e187260118df371d803d39c9e698634aa
Tim Fuller <tim@nikephoros.(none)>
2014-03-02 22:07:02 -0700

    new material db form

--- materials/library/mmats.py
+++ materials/library/mmats.py
@@ -4,9 +4,50 @@ This file is used only during the setup phase to build and install materials.
 
 """
 import os
+from materials.material import _Material
+
 D = os.path.dirname(os.path.realpath(__file__))
 
-NAMES = ("idealgas", "mnrv", "plastic", "elastic", "pyelastic")
+d = os.path.join(D, "idealgas")
+material = {"source_files": None,
+            "interface_file": os.path.join(d, "idealgas.py"),
+            "class_name": "IdealGas"}
+idealgas = _Material("idealgas", **material)
+
+d = os.path.join(D, "MooneyRivlin")
+source_files = [os.path.join(d, f) for f in ("mnrv.f90", "mnrv.pyf")]
+material = {"source_files": source_files,
+            "interface_file": os.path.join(d, "mnrv.py"),
+            "requires_lapack": True, "class_name": "MooneyRivlin"}
+mnrv = _Material("mnrv", **material)
+
+d = os.path.join(D, "plastic")
+source_files = [os.path.join(d, f) for f in
+                ("plastic_interface.f90", "plastic.f90", "plastic.pyf")]
+material = {"source_files": source_files,
+            "interface_file": os.path.join(d, "plastic.py"),
+            "class_name": "Plastic"}
+plastic = _Material("plastic", **material)
+
+# Elastic
+d = os.path.join(D, "elastic")
+source_files = [os.path.join(d, f) for f in
+                ("elastic_interface.f90", "elastic.f90", "elastic.pyf")]
+material = {"source_files": source_files,
+            "interface_file": os.path.join(d, "elastic.py"),
+            "class_name": "Elastic"}
+elastic = _Material("elastic", **material)
+
+d = os.path.join(D, "pyelastic")
+material = {"source_files": None,
+            "interface_file": os.path.join(d, "pyelastic.py"),
+            "class_name": "PyElastic"}
+pyelastic = _Material("pyelastic", **material)
+
+
+NAMES = {"idealgas": idealgas, "mnrv": mnrv, "plastic": plastic,
+         "elastic": elastic, "pyelastic": pyelastic}
+
 
 def conf(name=None):
     """Return the material configurations for building
@@ -25,51 +66,7 @@ def conf(name=None):
         return
 
     try:
-        nidx = [n.lower() for n in NAMES].index(name.lower())
+        return NAMES[name.lower()]
     except ValueError:
         print "{0}: unknown material".format(name)
         return
-
-    if nidx == 0:
-        # Ideal Gas
-        d = os.path.join(D, "idealgas")
-        material = {"source_files": None, "include_dir": d,
-                    "interface_file": os.path.join(d, "idealgas.py"),
-                    "class": "IdealGas"}
-
-
-    elif nidx == 1:
-        # Mooney Rivlin
-        d = os.path.join(D, "MooneyRivlin")
-        source_files = [os.path.join(d, f) for f in ("mnrv.f90", "mnrv.pyf")]
-        material = {"source_files": source_files, "include_dir": d,
-                    "interface_file": os.path.join(d, "mnrv.py"),
-                    "requires_lapack": True, "class": "MooneyRivlin"}
-
-    elif nidx == 2:
-        # Plastic
-        d = os.path.join(D, "plastic")
-        source_files = [os.path.join(d, f) for f in
-                        ("plastic_interface.f90", "plastic.f90", "plastic.pyf")]
-        material = {"source_files": source_files, "include_dir": d,
-                    "interface_file": os.path.join(d, "plastic.py"),
-                    "class": "Plastic"}
-    elif nidx == 3:
-        # Elastic
-        d = os.path.join(D, "elastic")
-        source_files = [os.path.join(d, f) for f in
-                        ("elastic_interface.f90", "elastic.f90", "elastic.pyf")]
-        material = {"source_files": source_files, "include_dir": d,
-                    "interface_file": os.path.join(d, "elastic.py"),
-                    "class": "Elastic"}
-    elif nidx == 4:
-        # python Elastic
-        d = os.path.join(D, "pyelastic")
-        material = {"source_files": None, "include_dir": d,
-                    "interface_file": os.path.join(d, "pyelastic.py"),
-                    "class": "PyElastic"}
-
-
-
-
-    return material
--- materials/material.py
+++ materials/material.py
@@ -8,176 +8,254 @@ from core.io import Error1
 from utils.impmod import load_file
 
 D = os.path.dirname(os.path.realpath(__file__))
+def cout(string):
+    sys.stdout.write(string + "\n")
 
 
-def gather_materials(mats_to_build):
-    """Gather all of the matmodlab materials
+class _Material:
+    """Class to hold some material meta data"""
+    def __init__(self, name, **kwargs):
+        self.source_files = None
+        self.requires_lapack = False
+        self.include_dir = None
+        self.name = name
+        for (k, v) in kwargs.items():
+            if k == "class": k = "class_name"
+            setattr(self, k, v)
+        self.python_model = not self.source_files
 
-    Parameters
-    ----------
-    mats_to_build : list or str
-      list of materials to build, or 'all' if all materials are to be built
+    def __getitem__(self, attr):
+        return self.__dict__[attr]
 
-    """
-    build_all = mats_to_build == "all"
-    material_info = {}
+    def items(self):
+        return self.__dict__.items()
 
-    # --- builtin materials are all described in the mmats file
-    mats = load_file(os.path.join(D, "library/mmats.py"))
-    for name in mats.NAMES:
-        if not build_all and name not in mats_to_build:
-            continue
-        material_info[name] = mats.conf(name)
+    @property
+    def dirname(self):
+        return os.path.dirname(self.interface_file)
 
-    # --- user materials
-    for dirname in UMATS:
-        if not os.path.isdir(dirname):
-            cout("  *** warning: {0}: no such directory".format(dirname))
-            continue
-        if "umat.py" not in os.listdir(dirname):
-            cout("  *** warning: umat.py not found in {0}".format(dirname))
-            continue
-        filename = os.path.join(dirname, "umat.py")
-        umat = load_file(filename)
+    def instantiate_material(self, params, options):
+        """Instantiate the material model"""
+
+        mtlmod = load_file(self.interface_file)
+        mclass = getattr(mtlmod, self.class_name)
+
+        mat = mclass()
+        mat.setup_new_material(params)
+        mat.set_constant_jacobian()
+        mat.set_options(**options)
+
+        return mat
+
+    def create_material(matname, matparams, matopts):
+        """Create a material object from the material name
+
+        """
+        # Instantiate the material object
+        material = self.get_material_from_db(matname)
+        if material is None:
+            return
+        mtlmod = load_file(material.interface_file)
+        mclass = getattr(mtlmod, material.class_name)
+
+
+
+class MaterialDB(object):
+    """Holder for material info"""
+    _xmldb = None
+    db_file = F_MTL_MODEL_DB
+    def __init__(self, *materials):
+        """
+
+        Parameters
+        ----------
+        materials : list of _Material instances
+
+        """
+        self._materials = []
+        for m in materials:
+            if not isinstance(m, _Material):
+                raise TypeError("Materials must be _Material type")
+            if m in self._materials:
+                raise AttributeError("{0}: duplicate material name".format(m.name))
+            self._materials.append(m)
+
+    def __repr__(self):
+        return "MaterialDB({0})".format(", ".join(m.name for m in self._materials))
+
+    def __getitem__(self, name):
+        m = self.get(name)
+        if m is None:
+            raise ValueError("{0}: not in DB".format(name))
+        return m
+
+    def get(self, name):
+        for material in self._materials:
+            if name == material.name:
+                return material
+
+    def remove(self, material):
         try:
-            name = umat.NAME
-        except AttributeError:
-            print("  ***error: {0}: NAME not defined".format(filename))
-            continue
-
-        if not build_all and name not in mats_to_build:
-            continue
-
-        try:
-            conf = umat.conf()
+            self._materials.remove(material)
         except ValueError:
-            print("  ***error: {0}: failed to gather information".format(filename))
-            continue
-        except AttributeError:
-            print("  ***error: {0}: conf function not defined".format(filename))
-            continue
+            raise ValueError("MaterialDB.remove(material): material not in DB")
 
-        if name in material_info:
-            print("  ***warning: {0}: duplicate material name".format(name))
-        material_info[name] = conf
+    def __iter__(self):
+        return iter(self._materials)
 
-    return material_info
+    def __getitem__(self, name):
+        for m in self._materials:
+            if name == m.name:
+                return m
 
+    def __len__(self):
+        return len(self._materials)
 
-def get_material_from_db(matname, mtldb=[None]):
-    matname = matname.lower()
-    if mtldb[0] is None:
-        mtldb[0] = read_mtldb()
-    if mtldb[0] is None:
-        return None
-    material = mtldb[0].get(matname)
-    if material is None:
-        return None
+    def put(self, material):
+        for (i, m) in enumerate(self._materials):
+            if m.name == material.name:
+                self._materials[i] = material
+                break
+        else:
+            self._materials.append(material)
 
-    # instantiate the material to get param names
-    mtlmod = load_file(material[0])
-    mclass = getattr(mtlmod, material[1])
-    parse_table = mclass.param_parse_table()
-    del mclass
+    @property
+    def interface_files(self):
+        """Path to interface files"""
+        return [m.interface_file for m in self._materials]
 
-    return material[0], material[1], parse_table
+    @property
+    def materials(self):
+        """Path to interface files"""
+        return [m.name for m in self._materials]
 
+    @property
+    def path(self):
+        """Directory names of all materials, can be used to set sys.path"""
+        return [m.dirname for m in self._materials]
 
-def instantiate_material(matname):
-    """Create a material object from the material name
+    def get_material_from_db(cls, matname):
+        matname = matname.lower()
+        mtldb = cls.gen_from_xmldb()
+        for m in mtldb.materials:
+            if m.name == matname:
+                return m
+
+    def write_db(self, wipe=False):
+        """Write the F_MTL_MODEL_DB database file
+
+        """
+        if wipe and os.path.isfile(F_MTL_MODEL_DB):
+            os.remove(F_MTL_MODEL_DB)
+
+        # read in previously created db since it may differ from the current db
+        mats_from_db = _read_db()
+        if not mats_from_db:
+            mats_from_db = []
+
+        for mat in mats_from_db:
+            self.put(mat)
+
+        doc = xdom.Document()
+        root = doc.createElement("Materials")
+
+        for material in self._materials:
+            # create element
+            child = doc.createElement("Material")
+            for (key, val) in material.items():
+                child.setAttribute(key, "{0}".format(val))
+            root.appendChild(child)
+
+        doc.appendChild(root)
+        doc.writexml(open(F_MTL_MODEL_DB, "w"), addindent="  ", newl="\n")
+        doc.unlink()
+        return
+
+    @classmethod
+    def gen_from_xmldb(cls):
+        materials = _read_db()
+        return cls(*materials)
+
+    @classmethod
+    def gen_from_search(cls, mats_to_build="all"):
+        """Gather all of the matmodlab materials
+
+        Parameters
+        ----------
+        mats_to_build : list or str
+          list of materials to build, or 'all' if all materials are to be built
+
+        """
+        build_all = mats_to_build == "all"
+
+        materials = []
+
+        # --- builtin materials are all described in the mmats file
+        mmats = load_file(os.path.join(D, "library/mmats.py"))
+        for name in mmats.NAMES:
+            if not build_all and name not in mats_to_build:
+                continue
+            materials.append(mmats.conf(name))
+
+        # --- user materials
+        for dirname in UMATS:
+            if not os.path.isdir(dirname):
+                cout("  *** warning: {0}: no such directory".format(dirname))
+                continue
+            if "umat.py" not in os.listdir(dirname):
+                cout("  *** warning: umat.py not found in {0}".format(dirname))
+                continue
+            filename = os.path.join(dirname, "umat.py")
+            umat = load_file(filename)
+            try:
+                name = umat.NAME
+            except AttributeError:
+                cout("  ***error: {0}: NAME not defined".format(filename))
+                continue
+
+            if not build_all and name not in mats_to_build:
+                continue
+
+            try:
+                conf = umat.conf()
+            except ValueError:
+                cout("  ***error: {0}: failed to gather "
+                     "information".format(filename))
+                continue
+            except AttributeError:
+                cout("  ***error: {0}: conf function not defined".format(filename))
+                continue
+
+            materials.append(_Material(name, **conf))
+
+        return cls(*materials)
+
+def _read_db():
+    """Read the material xml db
 
     """
-    # Instantiate the material object
-    model = get_material_from_db(matname)
-    if model is None:
-        return
-    mtli, mtlc = model
-    mtlmod = load_file(mtli)
-    mclass = getattr(mtlmod, mtlc)
-    return mclass()
-
-
-def create_material(matname, matparams, matopts):
-    """Create a material object from the material name
-
-    """
-    # Instantiate the material object
-    model = get_material_from_db(matname)
-    if model is None:
-        return
-    mtli, mtlc, mtlp = model
-    mtlmod = load_file(mtli)
-    mclass = getattr(mtlmod, mtlc)
-    material = mclass()
-    material.setup_new_material(matparams)
-    material.set_constant_jacobian()
-    material.set_options(**matopts)
-    return material
-
-
-def read_mtldb():
-    """Read the F_MTL_MODEL_DB database file
-
-    """
-    if not os.path.isfile(F_MTL_MODEL_DB):
-        return
-
+    materials = []
     try:
         doc = xdom.parse(F_MTL_MODEL_DB)
-    except ExpatError:
-        os.remove(F_MTL_MODEL_DB)
-        return None
-
-    mtldb = {}
-    materials = doc.getElementsByTagName("Materials")[0]
-    for mtl in materials.getElementsByTagName("Material"):
+    except IOError:
+        return
+    mats = doc.getElementsByTagName("Materials")[0]
+    for mtl in mats.getElementsByTagName("Material"):
         name = str(mtl.attributes.getNamedItem("name").value).lower()
-        filepath = str(mtl.attributes.getNamedItem("filepath").value)
-        filepath = os.path.realpath(os.path.join(D, filepath))
-        if not os.path.isfile(filepath):
-            raise Error1("{0}: no such file".format(filepath))
-        mclass = str(mtl.attributes.getNamedItem("mclass").value)
-        mtldb[name] = (filepath, mclass)
+        ad = {}
+        for (key, val) in mtl.attributes.items():
+            if key == "name": continue
+            try:
+                ad[key] = eval(val)
+            except (NameError, SyntaxError):
+                ad[key] = "{0}".format(val)
 
-    return mtldb
-
-
-def write_mtldb(built_mtls, wipe=False):
-    """Write the F_MTL_MODEL_DB database file
-
-    Parameters
-    ----------
-    built_mtls : list
-        list of (name, filepath, mclass, parameters) tuples of built
-        materials, where name is the model name, filepath is the path to its
-        interface file, mclass the name of the material class, and parameters
-        is a list of ordered parameter names.
-
-    """
-    if wipe and os.path.isfile(F_MTL_MODEL_DB):
-        os.remove(F_MTL_MODEL_DB)
-    mtldb = read_mtldb()
-    if mtldb is None:
-        mtldb = {}
-
-    for name, info in built_mtls.items():
-        filepath = info["interface_file"]
-        mclass = info["class"]
-        mtldb[name] = (filepath, mclass)
-
-    doc = xdom.Document()
-    root = doc.createElement("Materials")
-    doc.appendChild(root)
-
-    for (name, (filepath, mclass)) in mtldb.items():
-        # create element
-        child = doc.createElement("Material")
-
-        child.setAttribute("name", name)
-        child.setAttribute("filepath", filepath)
-        child.setAttribute("mclass", mclass)
-
-        root.appendChild(child)
-
-    doc.writexml(open(F_MTL_MODEL_DB, "w"), addindent="  ", newl="\n")
+        # instantiate the material to get param names
+        mtlmod = load_file(ad["interface_file"])
+        mtlmdl = getattr(mtlmod, ad["class_name"])
+        ad["parse_table"] = mtlmdl.param_parse_table()
+        del mtlmdl
+        materials.append(_Material(name, **ad))
     doc.unlink()
+    return materials
+
