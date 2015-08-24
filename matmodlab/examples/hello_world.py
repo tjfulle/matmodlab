0cef2c9e98f8204a8d355e10a27345e6a1809904
Scot Swan <mswan@sandia.gov>
2015-08-24 16:21:00 +0200

    Added hello_world tests for ipython and python

---
+++ matmodlab/examples/hello_world.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from matmodlab import *
+
+# initialize the job, request output in plaintext ("txt")
+mps = MaterialPointSimulator("hello_world", output="txt")
+
+# set up the linear elastic model "pyelastic"
+mps.Material("pyelastic", {"K": 1.0e10, "G": 1.0e9})
+
+# define the steps
+mps.StrainStep(components=(1., 0., 0.), frames=25)
+mps.StrainStep(components=(0., 1., 0.), frames=25)
+mps.StrainStep(components=(0., 0., 1.), frames=25)
+mps.StrainStep(components=(0., 0., 0.), frames=25)
+
+# run the simulation
+mps.run()
